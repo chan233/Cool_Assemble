2f8e50634b78558abba149c15d1c7879b3fef355
chan233 <1050181200@qq.com>
2023-08-27 20:06:37 +0800

    keystone

--- cap.py
+++ cap.py
@@ -1,8 +1,29 @@
 from  capstone import *
-# 通过capstone 写一个 arm32位下，thumb指令的反汇编函数
-def disasm_arm(code, addr, count):
-    md = Cs(CS_ARCH_ARM, CS_MODE_THUMB)
-    for i in range(count):
-        print(md.disasm(code[i], addr + i * 2).mnemonic, end=" ")
-        print(md.disasm(code[i], addr + i * 2).op_str)  
+from keystone import *
+'''
+push {r4, r5, r7, lr}
+mov r4, r1
+ldrb r1, [r1]
+cbz r1, #0x66
+blx #0x1f418
 
+'''
+
+
+CODE = b'\xB0\xB5\x0C\x46\x09\x78\x71\xB3\x1F\xF0\x06\xEA'
+
+
+def disam(code):
+    cs = Cs(CS_ARCH_ARM,CS_MODE_THUMB)
+    ks = Ks(KS_ARCH_ARM,KS_MODE_THUMB)
+    for i in cs.disasm(code,0,len(code)):      
+        
+        print(i.mnemonic+" "+i.op_str)
+        ins = ks.asm(i.mnemonic+" "+i.op_str)
+        print("----------")
+       
+    
+
+
+
+disam(CODE)
--- hook.js
+++ hook.js
@@ -1,8 +1,30 @@
 
-function dis(address, number) {
+function disa(address, number) {
+    console.log('star disamble address : ',address);
     for (var i = 0; i < number; i++) {
         var ins = Instruction.parse(address);
-        console.log("address:" + address + "--dis:" + ins.toString());
+        // console.log('=====================');
+        // console.log("address: " + address);
+        // console.log("ins.size: " + ins.size);
+        // console.log("ins.mnemonic: " + ins.mnemonic);
+        // console.log("ins.opStr: " + ins.opStr);
+        // console.log("ins.operands: " + ins.operands);
+        // console.log("ins.regsRead: " + ins.regsRead);
+        // console.log("ins.regsWritten: " + ins.regsWritten);
+        // console.log("ins.groups: " + ins.groups);
+       
+
+
+        // ins.size：该指令的大小
+        // ins.mnemonic：指令助记符的字符串表示形式
+        // ins.opStr：指令操作数字符串
+        // ins.operands：描述每个操作数的对象数组，每个操作数至少指定类型和值，还可能根据体系结构指定其他属性
+        // ins.regsRead：该指令隐式读取的寄存器名称数组
+        // ins.regsWritten：该指令隐式写入的寄存器名称数组
+        // ins.groups：该指令所属的组名数组
+        // instoString()：转换为人类可读字符串
+
+        console.log("instoString(): " + ins.toString());
         address = ins.next;
     }
 }
@@ -16,14 +38,19 @@ function hook() {
     var exports = targetso.enumerateExports()
     exports.forEach(function (ex) {
 
-        console.log(ex.name)
-        console.log(ex.address)
+
+        if (ex.name.includes("strstr") >= 1) {
+            console.log("export name:" + ex.name + "  export address" + ex.address)
+
+            disa(ex.address,10)
+        }
+
 
     }
     )
 
 }
-   
+
 
 
 
